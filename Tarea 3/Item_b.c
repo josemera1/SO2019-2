d8e7b1c6936f61b6a6e019bb267d5ab8c94b2dfc
LorenzoAndrees <lordandrees@gmail.com>
2019-12-16 06:38:54 -0300

    Uploading item b

---
+++ Tarea 3/Item_b.c
@@ -0,0 +1,129 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+#define M 300
+
+int array[M];
+
+uint64_t rdtsc(){
+    unsigned int lo,hi;
+    __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
+    return ((uint64_t)hi << 32) | lo;
+}
+
+int randomNumber(){
+    srand(rdtsc());
+    return (rand() % 10);
+}
+
+void init(){ //Initializing the matrix
+    int i,j,sign;
+    for(i=0;i<M;++i){
+        sign = randomNumber();
+        if(sign%2 == 0){
+            array[i] = randomNumber();
+        }
+        else{
+            array[i] = randomNumber() * (-1);
+        }
+    }
+}
+
+void printArray(){
+    int i,j;
+    printf("\n");
+    for(i = 0; i < M; ++i) printf("%d ",array[i]);
+    printf("\n");
+}
+
+void merge(int * array, int start, int end){
+    int i;
+    int middle = (start + end) / 2;
+    int temp_index = 0;
+    int * temp = malloc(sizeof(int) * (end - start + 1));
+    int left = start;
+    int right = middle + 1;
+    while((left <= middle) && (right <= end)) {
+        if (array[left] < array[right]) {
+            temp[temp_index] = array[left];
+            temp_index += 1;
+            left += 1;
+        }
+        else {
+            temp[temp_index] = array[right];
+            temp_index += 1;
+            right += 1;
+        }
+    }
+    while(left <= middle) {
+        temp[temp_index] = array[left];
+        temp_index += 1;
+        left += 1;
+    }
+    while(right <= end) {
+        temp[temp_index] = array[right];
+        temp_index += 1;
+        right += 1;
+    }
+    for(i = start; i <= end; i++) array[i] = temp[i - start];
+}
+
+//========================================CALCULO EN SERIE=================================================
+
+void mergeSortInSeries(int* array, int start, int end) {
+    if(start < end) {
+        int middle = (start + end) / 2;
+        mergeSortInSeries(array, start, middle);
+        mergeSortInSeries(array, middle + 1, end);
+        merge(array, start, end);
+    }
+}
+
+//======================================CALCULO EN PARALELO==============================================
+
+void mergeSortInParallel(int* array, int start, int end) {
+    if(start < end) {
+        int middle = (start + end) / 2;
+        #pragma omp parallel sections num_threads(4)
+            {
+            #pragma omp section
+            mergeSortInParallel(array, start, middle);
+            #pragma omp section
+            mergeSortInParallel(array, middle + 1, end);
+            }
+        merge(array, start, end);
+    }
+}
+
+int main(){
+    clock_t start_t;
+    double start,seconds;
+    printf("Inicializando array...\n");
+    init();
+    int * array2 = array;
+    //printf("Imprimiendo array original:\n");
+    //printArray();
+
+    printf("=========CALCULO EN SERIE===========\n");
+    start_t = clock();
+    mergeSortInSeries(array,0,M-1);
+    start_t = clock()- start_t;
+    seconds = (double)start_t/CLOCKS_PER_SEC;
+    //printArray();
+    //printf("El arreglo final es el siguiente:\n");
+    printf("El tiempo de ejecución sin OpenMP fue: %f segundos\n",seconds);
+
+    printf("=========CALCULO EN PARALELO============\n");
+    start = omp_get_wtime();
+    mergeSortInParallel(array2,0,M-1);
+    start = omp_get_wtime() - start;
+    //printf("El arreglo final es el siguiente:\n");
+    //printArray();
+    printf("El tiempo de ejecución con OpenMP fue: %f segundos.\n",start);
+
+    return 0;
+}
